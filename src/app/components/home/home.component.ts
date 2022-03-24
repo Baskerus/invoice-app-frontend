bb17df54e9737fe04dc84afdf36c6891d8cbaedb
Baskerus <baskerus@gmail.com>
2022-03-24 18:10:01 +0100

    refactored invoices total component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,7 +7,6 @@ import { Invoice } from 'src/app/interfaces/Invoice';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
   @Output() triggerRender = new EventEmitter();
   @Input() invoices: Invoice[];
   @Input() sidebar;
@@ -16,6 +15,14 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  numberOfInvoices() {
+    if (this.invoices.length === 1) {
+      return 'There is only one invoice';
+    } else if (this.invoices.length === 0) {
+      return 'No invoices to display';
+    }
+    return `There are ${this.invoices.length} invoices.`
+  }
 
   emitTriggerRender() {
     this.triggerRender.emit();
