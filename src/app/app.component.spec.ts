b8529387d724e9dabe21ccd436557fc9849e5731
Baskerus <baskerus@gmail.com>
2022-03-24 09:05:43 +0100

    unit testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,68 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { InvoiceService } from './services/invoice.service';
+import { from } from 'rxjs';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
-    }).compileComponents();
+  let component: AppComponent;
+  let service: InvoiceService;
+
+  beforeEach(() => {
+    service = new InvoiceService(null);
+    component = new AppComponent(service);
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  it('should get invoices from the server ', () => {
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'rbc-demo app is running!'
-    );
+    // Arrange
+    const invoices = [
+      {
+        id: 195,
+        name: 'Basker Onian',
+        email: 'ttt@ttt.com',
+        address: 'B6 Pencil Way',
+        city: 'Worcestershire',
+        code: 94105,
+        country: 'United Kingdom',
+        dueDate: '2022-03-18',
+        description: 'fdsfdf',
+        total: 1.33,
+        isPaid: false,
+      },
+      {
+        id: 196,
+        name: 'Basker Onian',
+        email: 'ttt@ttt.com',
+        address: 'B6 Pencil Way',
+        city: 'Worcestershire',
+        code: 94105,
+        country: 'United Kingdom',
+        dueDate: '2022-03-03',
+        description: '23',
+        total: 23.0,
+        isPaid: true,
+      },
+      {
+        id: 200,
+        name: 'Basker Onian',
+        email: 'ttt@ttt.com',
+        address: 'B6 Pencil Way',
+        city: 'Worcestershire',
+        code: 94105,
+        country: 'United Kingdom',
+        dueDate: '2022-03-19',
+        description: 'dfsdf',
+        total: 23.22,
+        isPaid: true,
+      },
+    ];
+
+    spyOn(service, 'getInvoices').and.callFake(() => {
+      return from([invoices]);
+    });
+    // Act - makes the call
+    component.ngOnInit();
+
+    // Assert
+    expect(component.invoices).toEqual(invoices);
   });
 });
--- src/app/components/invoice/invoice.component.spec.ts
+++ src/app/components/invoice/invoice.component.spec.ts
@@ -1,25 +1,51 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { InvoiceService } from 'src/app/services/invoice.service';
 import { InvoiceComponent } from './invoice.component';
 
-describe('InvoiceComponent', () => {
+let invoice = describe('Invoice Component', () => {
   let component: InvoiceComponent;
-  let fixture: ComponentFixture<InvoiceComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ InvoiceComponent ]
-    })
-    .compileComponents();
-  });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(InvoiceComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new InvoiceComponent(null, null);
+    component.invoice = {
+      id: 195,
+      name: 'Basker Onian',
+      email: 'ttt@ttt.com',
+      address: 'B6 Pencil Way',
+      city: 'Worcestershire',
+      code: 94105,
+      country: 'United Kingdom',
+      dueDate: '2022-03-18',
+      description: 'fdsfdf',
+      total: 1.33,
+      isPaid: false,
+    };
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display picked date in proper format', () => {
+    // Arrange
+    component.invoice.dueDate = "2023-04-08"
+    // Act
+    component.ngOnInit()
+    // Assert
+    expect(component.displayDate).toBe('08-04-2023');
+  });
+
+  it('should display initial date in proper format', () => {
+    // Arrange
+    let pickerDate = '2022/11/03';
+    // Act
+    component.handlePicker(pickerDate);
+    // Assert
+    expect(component.displayDate).toBe('03-11-2022');
+    expect(component.isDirty).toBe(true);
+  });
+
+  it('should switch paid status', () => {
+    // Arrange
+    let prevStatus = component.invoice.isPaid;
+    // Act
+    component.handleStatusButton();
+    // Assert
+    expect(component.invoice.isPaid).toBe(!prevStatus);
   });
 });
